38c8b8402ce74ea5b0e1706edc3eacfce067fb1c
introlist <gilflo01@hotmail.com>
2017-11-10 07:41:24 -0600

    Finished subs

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ var filter1 = "";
 var filter2 = "";
 var noInputFilter;
 var originalNoFilter;
+var responseContents;
+var jsonResponse;
+var jsonString;
 window.onload = function () {
     opcionesAreas();
     noInputFilter = document.getElementById("cell-filtros").innerHTML;
     originalNoFilter = document.getElementById("cell-filtros").innerHTML;
     generarOpciones();
+    //getJsonAreas();
+    generateFirstCombo();
+    //generateSecondCombo();
 };
+
+
 function generarOpciones() {
     var e = document.getElementById("repositorio");
     repo = e.options ? e.options[e.selectedIndex].value : repo;
@@ -310,9 +318,148 @@ function setURL() {
     document.getElementById("estatus").innerHTML = "";
 }
 
-function goToURL() {
-    window.open(document.getElementById("respuesta").value);
+function showURL(urlCons) {
 
+    document.getElementById("respuesta").value = urlCons;
+}
+
+
+function generateFirstCombo() {
+	var responseContents = "";
+	const proxyurl = "https://cors-anywhere.herokuapp.com/"; // <- Esta madre hace la magia
+	const url = "http://catalogs.repositorionacionalcti.mx/webresources/areacono"
+	fetch(proxyurl + url) 
+	.then(response => response.text())
+	.then(contents => woosh(contents))
+	.catch(console.log("Can’t access " + url + " response. Blocked by browser?"))
+    //generateFirstCombo();
+    showURL(url);
+}
+
+function generateSecondCombo(areasConocimiento) {
+    var selectedText = areasConocimiento.options[areasConocimiento.selectedIndex].innerHTML;
+    var selectedValue = areasConocimiento.value;
+    console.log(selectedValue);
+	var responseContents = "";
+	const proxyurl = "https://cors-anywhere.herokuapp.com/"; // <- Esta madre hace la magia
+	const url = "http://catalogs.repositorionacionalcti.mx/webresources/campocono/byArea/"+selectedValue;
+	fetch(proxyurl + url) 
+	.then(response => response.text())
+	.then(contents => woosh2(contents))
+	.catch(console.log("Can’t access " + url + " response. Blocked by browser?"))
+    //generateFirstCombo();
+    showURL(url);
+}
+
+function generateThirdCombo(camposConocimiento) {
+    var selectedText = camposConocimiento.options[camposConocimiento.selectedIndex].innerHTML;
+    var selectedValue = camposConocimiento.value;
+    console.log(selectedValue);
+	var responseContents = "";
+	const proxyurl = "https://cors-anywhere.herokuapp.com/"; // <- Esta madre hace la magia
+	const url = "http://catalogs.repositorionacionalcti.mx/webresources/disciplinacono/byCampo/"+selectedValue;
+    console.log(url);
+    fetch(proxyurl + url)
+	.then(response => response.text())
+	.then(contents => woosh3(contents))
+	.catch(console.log("Can’t access " + url + " response. Blocked by browser?"))
+    //generateFirstCombo();
+    showURL(url);
+}
+function generateForthCombo(disciplinasConocimiento) {
+    var selectedText = disciplinasConocimiento.options[disciplinasConocimiento.selectedIndex].innerHTML;
+    var selectedValue = disciplinasConocimiento.value;
+    console.log(selectedValue);
+    var responseContents = "";
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"; // <- Esta madre hace la magia
+    const url = "http://catalogs.repositorionacionalcti.mx/webresources/subdisciplinacono/byDisciplina/"+selectedValue;
+    console.log(url);
+    fetch(proxyurl + url)
+        .then(response => response.text())
+.then(contents => woosh4(contents))
+.catch(console.log("Can’t access " + url + " response. Blocked by browser?"))
+    //generateFirstCombo();
+    showURL(url);
+}
+
+function goToURL() {
+	var responseContents = "";
+	const proxyurl = "https://cors-anywhere.herokuapp.com/"; // <- Esta madre hace la magia
+	const url = document.getElementById("respuesta").value; 
+	fetch(proxyurl + url) 
+	.then(response => response.text())
+	.then(contents => woosh(contents))
+	.catch(console.log("Can’t access " + url + " response. Blocked by browser?"))
+}
+
+function woosh(contents){
+	responseContents = contents
+    //console.log("\{\"campos\":"+responseContents+"\}")
+	jsonResponse = JSON.parse("\{\"campos\":"+responseContents+"\}");
+    console.log(contents)
+	//console.log("\{\"campos\":"+JSON.stringify(jsonResponse)+"\}");
+    //jsonString = "\{\"campos\":"+JSON.stringify(jsonResponse)+"\}";
+     var listItems = '<option selected="selected" value="0">- Áreas de conocimiento -</option>';
+ 
+      for (var i = 0; i < jsonResponse.campos.length; i++) {
+             listItems += "<option value='" + jsonResponse.campos[i].idArea + "'>" + jsonResponse.campos[i].descripcion + "</option>";
+         }
+ 
+         $("#areasConocimiento").html(listItems);
+}
+
+function woosh2(contents){
+	responseContents = contents
+    //console.log("\{\"campos\":"+responseContents+"\}")
+	jsonResponse = JSON.parse("\{\"campos\":"+responseContents+"\}");
+    console.log(contents)
+	//console.log("\{\"campos\":"+JSON.stringify(jsonResponse)+"\}");
+    //jsonString = "\{\"campos\":"+JSON.stringify(jsonResponse)+"\}";
+     var listItems = '<option selected="selected" value="0">- Campos de conocimiento -</option>';
+ 
+      for (var i = 0; i < jsonResponse.campos.length; i++) {
+             listItems += "<option value='" + jsonResponse.campos[i].idCampo + "'>" + jsonResponse.campos[i].descripcion + "</option>";
+         }
+ 
+         $("#camposConocimiento").html(listItems);
+}
+
+function woosh3(contents){
+    responseContents = contents
+    //console.log("\{\"campos\":"+responseContents+"\}")
+    jsonResponse = JSON.parse("\{\"campos\":"+responseContents+"\}");
+    console.log(contents)
+    //console.log("\{\"campos\":"+JSON.stringify(jsonResponse)+"\}");
+    //jsonString = "\{\"campos\":"+JSON.stringify(jsonResponse)+"\}";
+    var listItems = '<option selected="selected" value="0">- Disciplinas de conocimiento -</option>';
+
+    for (var i = 0; i < jsonResponse.campos.length; i++) {
+        listItems += "<option value='" + jsonResponse.campos[i].idDisciplina + "'>" + jsonResponse.campos[i].descripcion + "</option>";
+    }
+
+    $("#disciplinasConocimiento").html(listItems);
+}
+
+
+function woosh4(contents){
+    responseContents = contents
+    //console.log("\{\"campos\":"+responseContents+"\}")
+    jsonResponse = JSON.parse("\{\"campos\":"+responseContents+"\}");
+    console.log('Subdisciplinas\n' +contents)
+    //console.log("\{\"campos\":"+JSON.stringify(jsonResponse)+"\}");
+    //jsonString = "\{\"campos\":"+JSON.stringify(jsonResponse)+"\}";
+    var listItems = '<option selected="selected" value="0">- Subdisciplinas de conocimiento -</option>';
+
+    for (var i = 0; i < jsonResponse.campos.length; i++) {
+        listItems += "<option value='" + jsonResponse.campos[i].idSubdisciplina + "'>" + jsonResponse.campos[i].descripcion + "</option>";
+    }
+
+    $("#subdisciplinasConocimiento").html(listItems);
+}
+
+
+function printInConsole(response){
+	print(response)
 }
 
 function copyURL() {
